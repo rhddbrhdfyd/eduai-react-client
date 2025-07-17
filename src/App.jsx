da250d66a6c7c69d45153b14941295387f0a857a
rhddbrhdfyd <jjklqwe16@gmail.com>
2025-07-17 11:43:40 +0900

    작업 내용

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import RoutesComponent from "./routes/index.jsx"; // ✅
+//src\App.jsx
+import React from "react";
+
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./routes";
 
 export default function App() {
   return (
     <Router>
-      <RoutesComponent /> {/* ✅ 여기서도 이름 일치 */}
+      <Routes />
     </Router>
   );
 }
